ac1f222607d763f2445b902e9a1e6ad10f48be6a
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-12-17 20:30:51 -0300

    sty: book form modal, close botton

--- divisio-read-webapp/src/components/forms/BookForm/index.js
+++ divisio-read-webapp/src/components/forms/BookForm/index.js
@@ -5,6 +5,7 @@ import './styles/book-form.scss';
 
 import Input from '../../ui/Input';
 import SelectBookStatus from '../../ui/SelectBookStatus';
+import IcoMoon from 'react-icomoon';
 
 class BookForm extends Component {
 
@@ -26,18 +27,20 @@ class BookForm extends Component {
   render() {
     return (
       <div className="book-form">
-        <div>
-          <button>X</button>
+        <div className="book-form__close-wrapper">
+          <button className="book-form__close-btn">
+            <IcoMoon icon="cross" />
+          </button>
         </div>
         <form className="book-form__wrapper" onSubmit={this.handleSaveBook}>
           <div className="form-group">
-            <Input label="Título"  onChange={title => this.setState({ title })} />
+            <Input label="Título" onChange={title => this.setState({ title })} />
           </div>
           <div className="form-group">
-            <Input label="Autor"  onChange={author => this.setState({ author })} />
+            <Input label="Autor" onChange={author => this.setState({ author })} />
           </div>
           <div className="form-group">
-            <Input label="Páginas"  onChange={pages => this.setState({ pages })} />
+            <Input label="Páginas" onChange={pages => this.setState({ pages })} />
           </div>
           <div className="form-group">
             <SelectBookStatus value='fechado' onChange={status => this.setState({ status })} />
@@ -46,9 +49,9 @@ class BookForm extends Component {
             <button className="btn">Salvar</button>
           </div>
         </form>
-        
-          <button className="btn-float">+</button>
-        
+
+        <button className="btn-float">+</button>
+
       </div>
 
     );
--- divisio-read-webapp/src/components/ui/BookStatusIcon/index.js
+++ divisio-read-webapp/src/components/ui/BookStatusIcon/index.js
@@ -2,29 +2,14 @@ import React from 'react';
 import IcoMoon from 'react-icomoon';
 import './style/book-status-icon.scss';
 
-const white = '#ffffff';
-const black = '#000000';
-const orange = '#fb812c';
-const red = '#d10403';
-const blue = '#5f8dd3';
-
-/* 
-      <IcoMoon icon="book" />
-      <IcoMoon icon="checkmark" />
-      <IcoMoon icon="clock" />
-      <IcoMoon icon="eye" />
-      <IcoMoon icon="cross" />
-      <IcoMoon icon="plus" />
-
-      */
 const BookStatusIcon = ({ value, onChange, type }) => (
   <label className="book-status-icon">
-    <div className={`book-status-icon__btn book-status-icon__btn--${type}`}>
-      <IcoMoon icon={type} className="book-status-icon__icon" />
-      <input 
-        name="status" 
-        value="fechado" 
-        type="radio" 
+    <div className={`book-status-icon__btn book-status-icon__btn--${type || 'book'}`}>
+      <IcoMoon icon={type || 'book'} className="book-status-icon__icon" />
+      <input
+        name="status"
+        value="fechado"
+        type="radio"
         className="book-status-icon__checkbox"
       />
     </div>
@@ -33,3 +18,12 @@ const BookStatusIcon = ({ value, onChange, type }) => (
 );
 
 export default BookStatusIcon;
+
+/* 
+<IcoMoon icon="book" />
+<IcoMoon icon="checkmark" />
+<IcoMoon icon="clock" />
+<IcoMoon icon="eye" />
+<IcoMoon icon="cross" />
+<IcoMoon icon="plus" />
+*/
